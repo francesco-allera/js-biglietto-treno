18619207656326c4b9a245344a36ab34e25cc86f
Francesco <francescoallera@gmail.com>
2021-01-25 23:10:44 +0100

    Exercise completed

--- js/script.js
+++ js/script.js
@@ -1,13 +1,48 @@
+var kilometri = parseInt(prompt('Inserisci il numero di kilometri che dovrai percorrere!'));
+
+var priceTicket = kilometri * 0.21;
+
+if (isNaN(kilometri)) {
+
+  alert('Riprova inserendo un numero');
+
+} else {
+
+  var age = parseInt(prompt('Bene, ora inserisci la tua età!'));
+
+  if (isNaN(age)) {
+
+    alert('Riprova inserendo un numero');
+
+  } else {
+
+    if (age < 18) {
+      priceTicket *= 0.8;
+    }
+
+    if (age > 65) {
+      priceTicket *= 0.6;
+    }
+
+    document.getElementById('ticket-price').innerHTML = 'Il prezzo del tuo biglietto è di ' + priceTicket.toFixed(2) + ' euro';
+  }
+}
+
+/* Prima Consegna:
 var distance = prompt('Hey, quanti kilometri devi fare?');
 
 if (isNaN(distance)) {
+
   alert('Puoi inserire solo numeri. Riprova!');
+
 } else {
 
   var age = prompt('Ok, ora... Quanti anni hai?');
 
   if (isNaN(age)) {
+
     alert('Puoi inserire solo numeri. Riprova!');
+
   } else {
 
     var price = parseInt(distance) * 0.21;
@@ -26,4 +61,4 @@ if (isNaN(distance)) {
 
     document.getElementById('ticketPrice').innerText = ('Bene! Il prezzo del tuo biglietto è di ' + finalPriceFixed + ' euro');
   }
-}
+} */
